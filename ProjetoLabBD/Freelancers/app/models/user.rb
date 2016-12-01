27b0382806f41b494f9927f39753ef84e3365288
HugoMitsumori <hugo.mitsumori@hotmail.com>
2016-12-01 14:28:23 -0200

    neo4j

--- ProjetoLabBD/Freelancers/app/models/user.rb
+++ ProjetoLabBD/Freelancers/app/models/user.rb
@@ -1,2 +1,9 @@
-class User < ApplicationRecord
+class User 
+	include Neo4j::ActiveNode
+	property :login, type: String
+	property :email, type: String
+	property :name, type: String
+	property :cpf, type: String
+	property :age, type: Integer
+	property :profession, type: String
 end
