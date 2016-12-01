384b8a665376de6a107d8f7995654da9487d1419
Luan Torres <luantorres95@hotmail.com>
2016-12-01 01:03:14 -0200

    Fixed creation SQL

--- ProjetoLabBD/criacao_banco.sql
+++ ProjetoLabBD/criacao_banco.sql
@@ -1,4 +1,3 @@
-
 CREATE TABLE usuario(
 	login VARCHAR(254) PRIMARY KEY,
 	email VARCHAR(254) UNIQUE NOT NULL,
@@ -21,23 +20,21 @@ CREATE TABLE seguidor(
 );
 
 CREATE TABLE problema(
-	id INT UNIQUE NOT NULL,
+	id INT UNIQUE PRIMARY KEY,
 	criador VARCHAR(254) NOT NULL,
 	descricao VARCHAR(254),
 	categoria VARCHAR(254),
 	prioridade INT NOT NULL,
-	PRIMARY KEY (id, criador),
 	FOREIGN KEY (criador) REFERENCES usuario(login)
 		ON DELETE RESTRICT
 		ON UPDATE CASCADE
 );
 
 CREATE TABLE solucao(
-	id INT UNIQUE NOT NULL,
+	id INT UNIQUE PRIMARY KEY,
 	criador VARCHAR(254) NOT NULL,
 	id_problema INT UNIQUE NOT NULL,
 	descricao VARCHAR(254),
-	PRIMARY KEY (id, criador, id_problema),
 	FOREIGN KEY (criador) REFERENCES usuario(login)
 		ON DELETE RESTRICT
 		ON UPDATE CASCADE,
@@ -60,10 +57,9 @@ CREATE TABLE empresa(
 );
 
 CREATE TABLE contratante(
-	login VARCHAR(254) UNIQUE NOT NULL,
-	cnpj_empresa VARCHAR(14) UNIQUE NOT NULL,
+	login VARCHAR(254) PRIMARY KEY,
+	cnpj_empresa VARCHAR(14) NOT NULL,
 	cargo VARCHAR(254),
-	PRIMARY KEY (login, cnpj_empresa),
 	FOREIGN KEY (login) REFERENCES usuario(login)
 		ON DELETE CASCADE
 		ON UPDATE CASCADE,
@@ -78,10 +74,9 @@ CREATE TABLE conhecimento(
 );
 
 CREATE TABLE conhecimentoFreelancer(
-	login_freelancer VARCHAR(254) UNIQUE NOT NULL,
-	nome_conhecimento VARCHAR(254) UNIQUE NOT NULL,
-	certificados VARCHAR(254),
-	PRIMARY KEY(login_freelancer, nome_conhecimento),
+	login_freelancer VARCHAR(254) NOT NULL,
+	nome_conhecimento VARCHAR(254) NOT NULL,
+	PRIMARY KEY (login_freelancer, nome_conhecimento),
 	FOREIGN KEY (login_freelancer) REFERENCES freelancer(login)
 		ON DELETE CASCADE
 		ON UPDATE CASCADE,
@@ -90,9 +85,20 @@ CREATE TABLE conhecimentoFreelancer(
 		ON UPDATE CASCADE
 );
 
+CREATE TABLE certificadoConhecimento(
+	nome_conhecimento VARCHAR(254) NOT NULL,
+	login_freelancer VARCHAR(254) NOT NULL,
+	nome_certificado VARCHAR(254) NOT NULL,
+	PRIMARY KEY (nome_conhecimento, login_freelancer, nome_certificado),
+	FOREIGN KEY (nome_conhecimento, login_freelancer) 
+		REFERENCES conhecimentoFreelancer (nome_conhecimento, login_freelancer)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
+);
+
 CREATE TABLE servico(
-	login_contratante VARCHAR(254) UNIQUE NOT NULL,
-	codigo_servico INT UNIQUE NOT NULL,
+	codigo_servico INT PRIMARY KEY,
+	login_contratante VARCHAR(254) NOT NULL,
 	id_solucao_geradora INT,
 	nome_servico VARCHAR(254) NOT NULL,
 	data_inicio DATE NOT NULL,
@@ -100,7 +106,6 @@ CREATE TABLE servico(
 	status VARCHAR(254) NOT NULL,
 	preco DECIMAL(17,2) NOT NULL,
 	avaliacao_final DECIMAL(4,1),
-	PRIMARY KEY (login_contratante, codigo_servico),
 	FOREIGN KEY (login_contratante) REFERENCES contratante(login)
 		ON DELETE RESTRICT
 		ON UPDATE CASCADE,
@@ -110,8 +115,8 @@ CREATE TABLE servico(
 );
 
 CREATE TABLE conhecimentoRequisitado(
-	nome_conhecimento VARCHAR(254) UNIQUE NOT NULL,
-	codigo_servico INT UNIQUE NOT NULL,
+	nome_conhecimento VARCHAR(254) NOT NULL,
+	codigo_servico INT NOT NULL,
 	PRIMARY KEY (nome_conhecimento, codigo_servico),
 	FOREIGN KEY (nome_conhecimento) REFERENCES conhecimento(nome)
 		ON DELETE CASCADE
@@ -123,9 +128,10 @@ CREATE TABLE conhecimentoRequisitado(
 
 -- DROP TABLE conhecimentoRequisitado;
 -- DROP TABLE servico;
+-- DROP TABLE certificadoConhecimento;
 -- DROP TABLE conhecimentoFreelancer;
 -- DROP TABLE conhecimento;
--- DROP TABLE empregado;
+-- DROP TABLE contratante;
 -- DROP TABLE empresa;
 -- DROP TABLE freelancer;
 -- DROP TABLE solucao;
